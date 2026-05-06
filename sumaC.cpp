a219805e2585fce50aed5141f5f311cd58c29d15
Raulgaray26 <raulgaray@uees.edu.ec>
2026-05-05 22:17:06 -0500

    Ejercicio en clase: suma de variables

---
+++ sumaC.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    int suma = a + b;
+    
+    printf("La suma de a y b es: %d\n", suma);
+}
---
+++ sumaPython.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+
+suma = a + b
+
+print("La suma de a y b es: ", suma)
